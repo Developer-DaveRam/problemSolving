cbc0a4732945d28f3625235977c40d1a24341341
Developer-Daveram <daveramskyraan@gmail.com>
2026-06-09 10:22:35 +0530

    changes

--- oddoreven.js
+++ oddoreven.js
@@ -1,10 +1,5 @@
 function checkOddEven(number){
-    if(number % 2 === 0 ){
-         return "even"
-    }
-    else
-
-    return "odd"
+  return  (number % 2 === 0 ) ?  "even" : "odd"
 }
 
 console.log(checkOddEven(3))
