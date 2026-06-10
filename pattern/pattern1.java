5ab8ea3ba2bef86364dd06f060b1e9565b973c8b
Developer-Daveram <daveramskyraan@gmail.com>
2026-06-10 10:56:55 +0530

    java

--- pattern/pattern1.java
+++ pattern/pattern1.java
@@ -1,21 +1,21 @@
-
 import java.util.Scanner;
 
-// Save this file as HelloWorld.java
-public class pattern1{
-    
+public class pattern1 {
+
     public static void main(String[] args) {
 
         Scanner nume = new Scanner(System.in);
 
+        System.out.print("Enter the number of stars: ");
+        int num = nume.nextInt();
 
-
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                    System.out.print("*");
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                System.out.print("*");
             }
-            System.err.println("");
+            System.out.println();
         }
 
+        nume.close();
     }
 }
